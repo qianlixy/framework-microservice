d8c453e8eaa417daafe44f1bc7522d8140420dfe
qianlixy <qianli_xy@163.com>
2017-11-14 22:45:12 +0800

    Add config server

--- framework-microservice-config-server/src/main/java/com/qianlixy/framework/microsevice/config/ConfigApplication.java
+++ framework-microservice-config-server/src/main/java/com/qianlixy/framework/microsevice/config/ConfigApplication.java
@@ -1,17 +1,17 @@
-package com.qianlixy.framework.microservice.zuul;
+package com.qianlixy.framework.microsevice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
-public class ZuulApplication {
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 	
 }
